cbb40cf4af6863fd9848c570b9075ec7a0a136c5
Julian Leon <juliandgleon@gmail.com>
2023-03-20 18:46:02 -0500

    Cart store added

---
+++ src/stores/cart.js
@@ -0,0 +1,105 @@
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
+import { LocalStorage } from 'quasar';
+
+export const useCartStore = defineStore( 'cart', () => {
+  /**************** STATE ****************/
+  const currentCart = ref( {
+    products: [],
+    total: 0
+  } )
+  /**************** STATE ****************/
+
+  /**************** HELPERS ****************/
+
+  function getCartFromLocalStorage () {
+    return JSON.parse(LocalStorage.getItem('cart'))
+  }
+
+  function setCartToLocalStorage () {
+    return LocalStorage.set( 'cart', JSON.stringify(currentCart.value) )
+  }
+
+  function getTotal () {
+    const products = currentCart._rawValue.products
+    const total = products.reduce((accumulator, object) => {
+      return accumulator + (object.price * object.quantity)
+    }, 0)
+    return( total )
+  }
+
+  function setTotals() {
+    currentCart.value.total = getTotal()
+  }
+
+  /**************** HELPERS ****************/
+
+  /**************** GETTERS ****************/
+
+  const getCurrentCart = computed( () => {
+    return( currentCart.value )
+  } )
+
+  /**************** GETTERS ****************/
+
+  /**************** ACTIONS ****************/
+
+  function addProduct ( product ) {
+    currentCart.value.products.push(product)
+
+    setCartToLocalStorage()
+    setTotals()
+  }
+
+  function removeProduct ( productIndex ) {
+    currentCart.value.products.splice(productIndex, 1)
+
+    setCartToLocalStorage()
+    setTotals()
+  }
+
+  function increaseProductQuantity ( productIndex ) {
+    currentCart.value.products[productIndex].quantity += 1
+
+    setCartToLocalStorage()
+    setTotals()
+  }
+
+  function decreaseProductQuantity ( productIndex ) {
+    currentCart.value.products[productIndex].quantity -= 1
+
+    if (currentCart.value.products[productIndex].quantity === 0) {
+      currentCart.value.products.splice(productIndex, 1)
+    }
+
+    setCartToLocalStorage()
+    setTotals()
+  }
+
+  function loadFromLocalStorage () {
+    const cartLocalStorage = getCartFromLocalStorage()
+    if (cartLocalStorage !== null && cartLocalStorage.products.length !== 0) {
+      currentCart.value = cartLocalStorage
+    }
+  }
+
+  function resetCurrentCart () {
+    currentCart.value.products = []
+    currentCart.value.total = 0
+
+    setCartToLocalStorage()
+  }
+
+  /**************** ACTIONS ****************/
+
+  return( {
+    getCurrentCart,
+    getTotal,
+    addProduct,
+    removeProduct,
+    increaseProductQuantity,
+    decreaseProductQuantity,
+    loadFromLocalStorage,
+    resetCurrentCart,
+  } )
+} )
